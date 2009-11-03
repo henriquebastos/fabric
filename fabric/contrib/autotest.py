7fe40d28392c2ea7a6718606806937f26741cc91
Henrique Bastos <henrique@bastos.net>
2009-11-03 09:28:35 -0200

    Fixed mistaken use of local() to os.system

--- fabric/contrib/autotest.py
+++ fabric/contrib/autotest.py
@@ -48,5 +48,5 @@ def autotest(command=None, sleep=1):
         new_val = checkSum()
         if val != new_val:
             val = new_val
-            local(command, capture=False)
+            os.system(command)
         time.sleep(sleep)
